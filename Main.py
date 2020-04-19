d7f30739fb2f8192c8b4def9c0be27e4049d6ed4
Pelumi <donpelumos@gmail.com>
2020-04-19 13:52:09 +0100

    - added two count to include two new feature columns.

--- Main.py
+++ Main.py
@@ -50,9 +50,9 @@ def main():
         f = FileUtil()
 
         keystroke_data = pd.read_csv("data/keystroke-data.csv")
-        data = keystroke_data.iloc[:, 0:38]
+        data = keystroke_data.iloc[:, 0:40]
         le = preprocessing.LabelEncoder()
-        #encoded_value = le.fit_transform(keystroke_data.iloc[:, 38:39])
+        #encoded_value = le.fit_transform(keystroke_data.iloc[:, 40:41])
         target = keystroke_data['CLASS']
         sample_text_row = pd.DataFrame.transpose(pd.DataFrame("0.078,0.101,0.105,0.093,0.096,0.080,0.093,0.091,0.093,0.077,0.085,0.064,0.082,0.094,0.091,0.080,0.106,0.162,0.099,0.081,0.065,0.083,0.095,0.092,0.079,0.102,0.133,0.030,0.036,0.090,0.000,0.087,0.000,0.092,0.101,0.000,2.936,5.818".split(",")))
         #sample_text_row = sample_text_row[0]
