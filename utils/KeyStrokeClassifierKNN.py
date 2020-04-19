5340cb86d3f1d62ea4c1ca92f2eae4fbaa6b26ba
Pelumi <donpelumos@gmail.com>
2020-04-19 01:57:45 +0100

    - added knn classification script and dataset folder.

--- utils/KeyStrokeClassifierKNN.py
+++ utils/KeyStrokeClassifierKNN.py
@@ -1,10 +1,10 @@
+import sys
 from sklearn.metrics import accuracy_score
 from utils.FileUtil import FileUtil
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
-from utils.KeyStrokeClassifierKNN import KeyStrokeClassifierKNN as Kc
 
 
 class KeyStrokeClassifierKNN:
@@ -17,6 +17,7 @@ class KeyStrokeClassifierKNN:
         of the dataset should be used as test in generating the knn model.
         It is any decimal between 0.0 to 1.0.
     -   The fourth argument is the size of 'n' neighbours to be used"""
+    __base_path = sys.path[0]
 
     def __init__(self, dataset_file_path, test_feature_string, knn_model_test_ratio, neighbour_size):
         self.dataset_file_path = dataset_file_path
@@ -28,7 +29,7 @@ class KeyStrokeClassifierKNN:
         keystroke_data = pd.read_csv(self.dataset_file_path)
         data = keystroke_data.iloc[:, 0:38]
         le = preprocessing.LabelEncoder()
-        encoded_value = le.fit_transform(keystroke_data.iloc[:, 38:39])
+        #encoded_value = le.fit_transform(keystroke_data.iloc[:, 38:39])
         target = keystroke_data['CLASS']
         sample_text_row = pd.DataFrame.transpose(pd.DataFrame(self.test_feature_string.split(",")))
 
@@ -40,38 +41,6 @@ class KeyStrokeClassifierKNN:
         knn_model.fit(data_train, target_train)
         pred = knn_model.predict(data_test)
         prediction = knn_model.predict(sample_text_row)
-        print("KNeighbors accuracy score : ", accuracy_score(target_test, pred))
-        print(str(prediction[0]))
+        #print("KNeighbors accuracy score : ", accuracy_score(target_test, pred))
+        #print(str(prediction[0]))
         return str(prediction[0])
-
-
-def main():
-    try:
-        f = FileUtil()
-
-        keystroke_data = pd.read_csv("data/keystroke-data.csv")
-        data = keystroke_data.iloc[:, 0:38]
-        le = preprocessing.LabelEncoder()
-        #encoded_value = le.fit_transform(keystroke_data.iloc[:, 38:39])
-        target = keystroke_data['CLASS']
-        sample_text_row = pd.DataFrame.transpose(pd.DataFrame("0.078,0.101,0.105,0.093,0.096,0.080,0.093,0.091,0.093,0.077,0.085,0.064,0.082,0.094,0.091,0.080,0.106,0.162,0.099,0.081,0.065,0.083,0.095,0.092,0.079,0.102,0.133,0.030,0.036,0.090,0.000,0.087,0.000,0.092,0.101,0.000,2.936,5.818".split(",")))
-        #sample_text_row = sample_text_row[0]
-        data_train, data_test, target_train, target_test = train_test_split(data, target, test_size=0.3, random_state=10)
-        neigh = KNeighborsClassifier(n_neighbors=3)
-        neigh.fit(data_train, target_train)
-        pred = neigh.predict(data_test)
-        pred2 = neigh.predict(sample_text_row)
-        print("KNeighbors accuracy score : ", accuracy_score(target_test, pred))
-        print(str(pred[0]))
-        f.readfile("demo.txt")
-
-    except Exception as e:
-        a = 4
-    file_path = "data/keystroke-data.csv"
-    test_string = "0.078,0.101,0.105,0.093,0.096,0.080,0.093,0.091,0.093,0.077,0.085,0.064,0.082,0.094,0.091,0.080,0.106,0.162,0.099,0.081,0.065,0.083,0.095,0.092,0.079,0.102,0.133,0.030,0.036,0.090,0.000,0.087,0.000,0.092,0.101,0.000,2.936,5.818"
-    customClassifier = Kc(file_path, test_string, 0.3, 3)
-    print(customClassifier.fetch_classification())
-    print("Main Method")
-
-
-main()
